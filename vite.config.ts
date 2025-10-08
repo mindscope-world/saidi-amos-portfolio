3bf9d8752fd8cce0ee4ac7ffa33c22703af7eb54
mindscope-world <ndirangup254@gmail.com>
2025-10-08 17:31:03 +0300

    Initital commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "clean-folio-aura.onrender.com"
+    ]
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
